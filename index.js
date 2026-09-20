90703219c39f4670e81db1dd125ab44bbceda6f0
Muneera Ibrahim <122372002+muneeracodes@users.noreply.github.com>
2026-09-20 15:26:53 +0500

    Stage 1: root and health endpoints

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        "name": "Task API",
+        "version": "1.0",
+        "endpoints": ["/tasks"]
+    });
+});
+
+app.get('/health',(req, res) => {
+    res.json({
+        "status": "OK"
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+}); 
